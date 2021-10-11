ecb742722b94208318d890c56ab0214d9c5c82a1
Michael Majdanski <35552662+mmajdanski@users.noreply.github.com>
2021-10-11 16:33:02 -0700

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<div class="bg-white">
+  <main class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div
+      class="relative z-10 flex items-baseline justify-between pt-24 pb-6 border-b border-gray-200"
+    >
+      <h1
+        class="text-4xl font-extrabold tracking-tight text-gray-900"
+        id="welcome-header"
+      >
+        Welcome, Janie Doe
+      </h1>
+    </div>
+
+    <section aria-labelledby="nav-heading" class="pt-6 pb-24">
+      <div
+        class="grid grid-cols-1 lg:grid-cols-4 gap-x-8 gap-y-10"
+        id="nav-area"
+      >
+        Navigation Area
+
+        <div class="lg:col-span-3">
+          <div
+            class="border-4 border-dashed border-gray-200 rounded-lg h-96 lg:h-full  transform transition duration-500 hover:scale-95"
+            id="content-area"
+          >
+            <span
+              class="text-lg font-mono font-extrabold uppercase text-green-900 hover:cursor-auto"
+              >Hello World</span
+            >
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</div>
+
+<style global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
